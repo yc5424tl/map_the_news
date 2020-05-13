23c6a3035fd44048ec1bd5c095d0943dbaad9d42
j <j@localhost.com>
2020-05-13 00:33:48 -0500

    chmod +x entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 #SEC_KEY=$( cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 32 )
 SEC_KEY=$( cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 32 )
 export SECRET_KEY=${SEC_KEY}
-gunicorn geodjango_news_map.wsgi:application --bind 0.0.0.0:"${PORT}"
+gunicorn mtn_core.wsgi:application --bind 0.0.0.0:"${PORT}"
