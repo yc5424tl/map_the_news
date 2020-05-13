8321c8cbce1729b4d91389a1b8cd98d3602a06aa
j <j@localhost.com>
2020-05-12 21:11:06 -0500

    fixed required CustomUser fields added django.contrib.gis to settings.INSTALLED_APPS

--- mtn_core/settings.py
+++ mtn_core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.gis',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
--- mtn_user/managers.py
+++ mtn_user/managers.py
@@ -2,11 +2,12 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
+
 class CustomUserManager(BaseUserManager):
     """
     Custom user model manager using email as unique identifier !username
     """
-    def _create_user(self, email, password, username, first_name, last_name, is_staff, is_superuser, **extra_fields):
+    def _create_user(self, email, password, username, is_staff, is_superuser, first_name=None, middle_initial=None, last_name=None, **extra_fields):
         if not email:
             raise ValueError(_('Email Required'))
         now = timezone.now()
@@ -18,6 +19,7 @@ class CustomUserManager(BaseUserManager):
             is_superuser=is_superuser,
             is_active=True,
             first_name=first_name,
+            middle_initial=middle_initial,
             last_name=last_name,
             last_login=now,
             date_joined=now,
@@ -27,8 +29,14 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password, username, first_name, last_name, **extra_fields):
-        return self._create_user(email, password, username, first_name, last_name, False, False, **extra_fields)
+    # def create_user(self, email, password, username, first_name=None, middle_initial=None, last_name=None, **extra_fields):
+    #     return self._create_user(email, password, username, first_name, middle_initial, last_name, False, False, **extra_fields)
 
-    def create_superuser(self, email, password, username, first_name, last_name, **extra_fields):
-        return self._create_user(email, password, username, first_name, last_name, True, True, **extra_fields)
+    def create_user(self, password, email, username, is_staff=False, is_superuser=False, first_name=None, middle_initial=None, last_name=None, **extra_fields):
+        return self._create_user(password, email, username, is_staff, is_superuser, first_name, middle_initial, last_name, **extra_fields)
+
+    def create_superuser(self, password, email, username, is_staff=True, is_superuser=True, first_name=None, middle_initial=None, last_name=None, **extra_fields):
+        return self._create_user(password, email, username, is_staff, is_superuser, first_name, middle_initial, last_name, **extra_fields)
+
+    # def create_superuser(self, email, password, username, first_name=None, middle_initial=None, last_name=None, **extra_fields):
+    #     return self._create_user(email, password, username, first_name, middle_initial, last_name, True, True, **extra_fields)
