8ea31e375542fc678e58c68891470404384a73ad
j <j@localhost.com>
2020-05-15 05:05:28 -0500

    docker-compose used to run local container for executing makemigrations and docker cp the directories to host to push to heroku

--- mtn_web/views.py
+++ mtn_web/views.py
@@ -272,7 +272,7 @@ def new_post(request):
 
 @login_required()
 def update_post(request, post_pk):
-    return render(request, "general/update_post.html")
+    return render(request, "general/update_post.html", {'post_pk': post_pk})
 
 
 @login_required()
