7c451bc6fa030a87cb0834a25e0b688a2a3acfc1
j <j@localhost.com>
2020-05-15 08:22:31 -0500

    automated all of migration and superuser creation
    currently exporting, resetting, and importing this and sifters data
    
    moving sifters db to the same as this db, requiring a reset, but will not be restricted by the row limit of free heroku db that is currently impacting mtn.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,14 @@ export SECRET_KEY=${SEC_KEY}
 echo 'Collecting Static Files'
 python3 manage.py collectstatic --no-input
 echo 'Applying Database Migrations'
-python3 manage.py migrate --noinput
+python3 manage.py makemigrations mtn_web
+python3 manage.py makemigrations mtn_user
+python3 manage.py migrate mtn_user --noinput
+python3 manage.py migrate mtn_web --noinput
+python3 manage.py migrate sessions --noinput
+python3 manage.py migrate admin --noinput
+python3
+python3 manage.py ensure_adminuser --username=$"SU_USERNAME" --email=$"SU_EMAIL" --password=$"SU_PASSWORD"
+
 echo 'Starting Server'
 gunicorn mtn_core.wsgi:application --bind 0.0.0.0:"${PORT}"
