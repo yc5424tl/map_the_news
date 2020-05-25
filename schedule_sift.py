4d4eac25b7fa9df3d430a631f68c3a93caea015e
j <j@localhost.com>
2020-05-25 13:40:00 -0500

    chmod +x release.sh chmod +x release.py

--- schedule_sift.py
+++ schedule_sift.py
@@ -1,4 +1,4 @@
-from mtn_sifter.sifter import sift, verify_base_cat, verify_base_src
+
 import os
 
 
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mtn_core.settings')
     import django
     django.setup()
+    from mtn_sifter.sifter import sift, verify_base_cat, verify_base_src
     verify_base_cat()
     verify_base_src()
     sift()
