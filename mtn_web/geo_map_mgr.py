94ee88e4eb688db93e2fa64393c1366e59e0a8d1
Jacob Boline <17325414+yc5424tl@users.noreply.github.com>
2020-05-26 10:12:50 -0500

    Update geo_map_mgr.py

--- mtn_web/geo_map_mgr.py
+++ mtn_web/geo_map_mgr.py
@@ -9,6 +9,9 @@ import pandas as pd
 import pycountry
 
 from django.conf import settings
+
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 from mtn_web.geo_data_mgr import GeoDataManager
 
 logger = logging.getLogger(__name__)
@@ -27,37 +30,43 @@ class GeoMapManager:
     def build_choropleth(
         self, argument, focus, geo_data_mgr: GeoDataManager
     ) -> (folium.Map, str) or None:
-        world_df = gp.read_file(geo_data_mgr.filename)
-        global_map = folium.Map(location=[0, 0], tiles="OpenStreetMap", zoom_start=3)
-        articles_per_country = pd.Series(geo_data_mgr.result_dict)
-        world_df["article_count"] = world_df["id"].map(articles_per_country)
-        world_df.head()
-        world_df.plot(column="article_count")
-        threshold_scale = self.get_threshold(articles_per_country)
-        folium.Choropleth(
-            geo_data=geo_data_mgr.json_data,
-            name="choropleth",
-            data=world_df,
-            columns=["id", "article_count"],
-            key_on="feature.id",
-            fill_color="Dark2",
-            bins=[
-                float(x) for x in threshold_scale
-            ],  # https://github.com/python-visualization/folium/issues/1130
-            fill_opacity=0.7,
-            line_opacity=0.2,
-        ).add_to(global_map)
-        # YlGrBu - RdYlGn - YlOrBr - RdYlBu - PuBuGn - YlOrRd
-        # Oranges - Greens -Purples - Reds - Greys - Blues
-        # Pastel1 - Pastel2 - Spectral - Set1 - Set2 - Set3 - Dark2
-        folium.TileLayer("stamenwatercolor", attr="attr").add_to(global_map)
-        folium.TileLayer("stamenterrain", attr="attr").add_to(global_map)
-        folium.TileLayer("cartodbpositron", attr="attr").add_to(global_map)
-        folium.TileLayer("OpenStreetMap", attr="attr").add_to(global_map)
-        folium.LayerControl().add_to(global_map)
-        filename = f"{datetime.ctime(datetime.now()).replace(' ', '_').replace(':', '-')}_{focus}_query_{argument}_choropleth_map.html"
-        global_map.save(CHORO_MAP_ROOT + filename)
-        return global_map, filename if global_map and filename else None
+        try:
+            geo_data_url = staticfiles_storage.url('js/geo_data.json')
+            world_df = gp.read_file(geo_data_url)
+            global_map = folium.Map(location=[0, 0], tiles="OpenStreetMap", zoom_start=3)
+            articles_per_country = pd.Series(geo_data_mgr.result_dict)
+            world_df["article_count"] = world_df["id"].map(articles_per_country)
+            world_df.head()
+            world_df.plot(column="article_count")
+            threshold_scale = self.get_threshold(articles_per_country)
+            folium.Choropleth(
+                geo_data=geo_data_mgr.json_data,
+                name="choropleth",
+                data=world_df,
+                columns=["id", "article_count"],
+                key_on="feature.id",
+                fill_color="Dark2",
+                bins=[
+                    float(x) for x in threshold_scale
+                ],  # https://github.com/python-visualization/folium/issues/1130
+                fill_opacity=0.7,
+                line_opacity=0.2,
+            ).add_to(global_map)
+            # YlGrBu - RdYlGn - YlOrBr - RdYlBu - PuBuGn - YlOrRd
+            # Oranges - Greens -Purples - Reds - Greys - Blues
+            # Pastel1 - Pastel2 - Spectral - Set1 - Set2 - Set3 - Dark2
+            folium.TileLayer("stamenwatercolor", attr="attr").add_to(global_map)
+            folium.TileLayer("stamenterrain", attr="attr").add_to(global_map)
+            folium.TileLayer("cartodbpositron", attr="attr").add_to(global_map)
+            folium.TileLayer("OpenStreetMap", attr="attr").add_to(global_map)
+            folium.LayerControl().add_to(global_map)
+            filename = f"{datetime.ctime(datetime.now()).replace(' ', '_').replace(':', '-')}_{focus}_query_{argument}_choropleth_map.html"
+            global_map.save(CHORO_MAP_ROOT + filename)
+            return global_map, filename if global_map and filename else None
+   
+        except (requests.exceptions.RequestException, FileNotFoundError) as e:
+            logger.log(level=logging.ERROR, msg=f"Error fetching mapping json: {e}")
+            return None
 
     @staticmethod
     def get_threshold(articles_per_country: [dict]) -> [int]:
