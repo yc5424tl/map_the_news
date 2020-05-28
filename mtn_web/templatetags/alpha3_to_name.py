5817e665ef5e1171d73b313472c8faad5ae721e6
j <j@localhost.com>
2020-05-28 05:39:45 -0500

    migration for result.articles_per_country

--- mtn_web/templatetags/alpha3_to_name.py
+++ mtn_web/templatetags/alpha3_to_name.py
@@ -6,4 +6,4 @@ register = template.Library()
 
 @register.filter()
 def alpha3_to_name(a3_code):
-    return (pycountry.countries.get(alpha_3=a3_code.upper())).name
+    return pycountry.countries.get(alpha_3=a3_code.upper()).name
