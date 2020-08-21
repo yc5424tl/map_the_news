a1e810d90674b1f176f647d008fe9e5b7da8b3f6
jrb <jacob.boline.mn@gmail.com>
2020-08-21 15:04:05 -0500

    merge conflict resolutions

--- mtn_sifter/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MtnSifterConfig(AppConfig):
-    name = "mtn_sifter"
--- release.sh
+++
@@ -1,3 +0,0 @@
-python3 manage.py migrate mtn_web
-python3 manage.py migrate sessions
-python3 manage.py migrate admin
